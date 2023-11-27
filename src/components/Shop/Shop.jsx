099ce5acd186766d99ec20205c187e5813746e82
shuvo-99 <anontoshuvo99@gmail.com>
2023-11-27 21:56:30 +0600

    shop page

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Shop.css";
+
+const Shop = () => {
+  return (
+    <div className="shop_container">
+      <div>
+        <h1>Product</h1>
+      </div>
+      <div>
+        <h1>Order</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
